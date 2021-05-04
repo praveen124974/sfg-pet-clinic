81421928a54ade40c26a01e03ac98bf5c3711d22
praveen124974 <praveenkumar.natarajan@walmart.com>
2021-05-03 19:50:00 -0500

    Initial Project Add

---
+++ src/test/java/pappu/springframework/sfgpetclinic/SfgPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package pappu.springframework.sfgpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
